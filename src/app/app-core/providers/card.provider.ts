8113f1504118e8cce27ccbc9ede96a0b12f4397d
kmilanovic <33121923+kmilanovic@users.noreply.github.com>
2023-06-23 01:23:28 +0200

    refactor: remove unused imports, add css property

--- src/app/app-core/providers/card.provider.ts
+++ src/app/app-core/providers/card.provider.ts
@@ -2,11 +2,9 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map, Observable } from "rxjs";
 import { CardModel, RulingModel } from "../../pages/card/model/dto/card.model";
-import { CARD_SEARCH_URL } from "../globals/global";
 import {environment} from "../../../environments/environment";
 import {ByIdCommand} from "../../pages/card/model/command/by-id.command";
 import {SearchCardCommand} from "../../pages/card/model/command/search-card.command";
-import {SetModel} from "../../pages/set/model/dto/set.model";
 import {SetCodeCommand} from "../../pages/set/model/command/set-code.command";
 import {SaveCardInSetCommand} from "../../pages/card/model/command/save-card-in-set.command";
 
